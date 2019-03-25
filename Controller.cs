330ebb29ae0484f8f4f7f2d76bcdf3b654c62b70
CarloCapuz <36867035+CarloCapuz@users.noreply.github.com>
2019-03-25 11:31:52 -0400

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace FolderSecurityPermission
+{
+    class Controller
+    {
+        // Function to validate drive letter
+        public static void ValidateDrive(ref string drive)
+        {
+            while (drive == null || drive == " ")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Drive cannot be empty. " +
+                    "\nPlease specify which drive to put your folder in: ");
+                drive = Console.ReadLine();
+            } // end while
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+        } // end ValidateDrive()
+
+        // Function to validate response for changing permissions (y/n)
+        public static void ValidateResponse3(ref string resp3)
+        {
+            while (resp3 != "y" && resp3 != "n")
+            {
+                Console.ForegroundColor = ConsoleColor.Red; // change text color to red
+                Console.Write("Invalid Selection \nDo you want to change the permissions? (y/n): ");
+                resp3 = Console.ReadLine();  // resp = response from the user
+            } // end while
+
+            Console.ForegroundColor = ConsoleColor.Green;
+        } // end ValidateResposne3()
+
+        // Function to validate the name of the input to make sure it's not null or empty
+        public static void ValidateName(ref string input)
+        {
+            while (input == null || input == " ")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Path cannot be empty. " +
+                    "\nPlease specify the name of your directory: ");
+                input = Console.ReadLine();
+            } // end while
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+        } // end ValidateName()
+
+        // Security Permissions
+        // FULL CONTROL
+        public static void SetFolderPermissionFullControl(string path)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(path);
+                var directorySecurity = directoryInfo.GetAccessControl();
+                var currentUserIdentity = WindowsIdentity.GetCurrent();
+                var fileSystemRule = new FileSystemAccessRule(currentUserIdentity.Name,
+                                                              FileSystemRights.FullControl,
+                                                              InheritanceFlags.ObjectInherit |
+                                                              InheritanceFlags.ContainerInherit,
+                                                              PropagationFlags.None,
+                                                              AccessControlType.Allow);
+
+                directorySecurity.AddAccessRule(fileSystemRule);
+                directoryInfo.SetAccessControl(directorySecurity);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Problem occured at {0}", e.ToString());
+            }
+
+        } // end SetFolderPermissionFullControl
+
+        // READ
+        public static void SetFolderPermissionRead(string path)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(path);
+                var directorySecurity = directoryInfo.GetAccessControl();
+                var currentUserIdentity = WindowsIdentity.GetCurrent();
+                var fileSystemRule = new FileSystemAccessRule(currentUserIdentity.Name,
+                                                              FileSystemRights.Read,
+                                                              InheritanceFlags.ObjectInherit |
+                                                              InheritanceFlags.ContainerInherit,
+                                                              PropagationFlags.None,
+                                                              AccessControlType.Allow);
+
+                directorySecurity.AddAccessRule(fileSystemRule);
+                directoryInfo.SetAccessControl(directorySecurity);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Problem occured at {0}", e.ToString());
+            }
+        } // end SetFolderPermissionRead
+
+        // WRITE
+        public static void SetFolderPermissionWrite(string path)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(path);
+                var directorySecurity = directoryInfo.GetAccessControl();
+                var currentUserIdentity = WindowsIdentity.GetCurrent();
+                var fileSystemRule = new FileSystemAccessRule(currentUserIdentity.Name,
+                                                              FileSystemRights.Write,
+                                                              InheritanceFlags.ObjectInherit |
+                                                              InheritanceFlags.ContainerInherit,
+                                                              PropagationFlags.None,
+                                                              AccessControlType.Allow);
+
+                directorySecurity.AddAccessRule(fileSystemRule);
+                directoryInfo.SetAccessControl(directorySecurity);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Problem occured at {0}", e.ToString());
+            }
+        } // end SetFolderPermissionWrite
+
+        // MODIFY
+        public static void SetFolderPermissionModify(string path)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(path);
+                var directorySecurity = directoryInfo.GetAccessControl();
+                var currentUserIdentity = WindowsIdentity.GetCurrent();
+                var fileSystemRule = new FileSystemAccessRule(currentUserIdentity.Name,
+                                                              FileSystemRights.Modify,
+                                                              InheritanceFlags.ObjectInherit |
+                                                              InheritanceFlags.ContainerInherit,
+                                                              PropagationFlags.None,
+                                                              AccessControlType.Allow);
+
+                directorySecurity.AddAccessRule(fileSystemRule);
+                directoryInfo.SetAccessControl(directorySecurity);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Problem occured at {0}", e.ToString());
+            }
+        } // end SetFolderPermissionModify
+
+        // READ AND EXECUTE
+        public static void SetFolderPermissionReadAndExecute(string path)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(path);
+                var directorySecurity = directoryInfo.GetAccessControl();
+                var currentUserIdentity = WindowsIdentity.GetCurrent();
+                var fileSystemRule = new FileSystemAccessRule(currentUserIdentity.Name,
+                                                              FileSystemRights.ReadAndExecute,
+                                                              InheritanceFlags.ObjectInherit |
+                                                              InheritanceFlags.ContainerInherit,
+                                                              PropagationFlags.None,
+                                                              AccessControlType.Allow);
+
+                directorySecurity.AddAccessRule(fileSystemRule);
+                directoryInfo.SetAccessControl(directorySecurity);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Problem occured at {0}", e.ToString());
+            }
+        } // end SetFolderPermissionReadAndExecute
+
+    } // end Controller class
+} // end namespace
---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Folder_Security_Permission
+{
+    partial class FSPform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxDrive = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxPermission = new System.Windows.Forms.ComboBox();
+            this.labelOutputPerm = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drive:";
+            // 
+            // comboBoxDrive
+            // 
+            this.comboBoxDrive.FormattingEnabled = true;
+            this.comboBoxDrive.Items.AddRange(new object[] {
+            "C",
+            "E",
+            "N",
+            "P",
+            "U",
+            "Z",
+            "Y"});
+            this.comboBoxDrive.Location = new System.Drawing.Point(199, 39);
+            this.comboBoxDrive.Name = "comboBoxDrive";
+            this.comboBoxDrive.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDrive.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Folder name:";
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(199, 74);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(214, 20);
+            this.textBoxFolder.TabIndex = 3;
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(24, 248);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 20);
+            this.labelOutput.TabIndex = 4;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreate.Location = new System.Drawing.Point(151, 191);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 5;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(338, 191);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Security Permission:";
+            // 
+            // comboBoxPermission
+            // 
+            this.comboBoxPermission.FormattingEnabled = true;
+            this.comboBoxPermission.Items.AddRange(new object[] {
+            "Full Control",
+            "Modify",
+            "Read & Execute",
+            "Read",
+            "Write",
+            " "});
+            this.comboBoxPermission.Location = new System.Drawing.Point(199, 111);
+            this.comboBoxPermission.Name = "comboBoxPermission";
+            this.comboBoxPermission.Size = new System.Drawing.Size(214, 21);
+            this.comboBoxPermission.TabIndex = 8;
+            // 
+            // labelOutputPerm
+            // 
+            this.labelOutputPerm.AutoSize = true;
+            this.labelOutputPerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutputPerm.Location = new System.Drawing.Point(24, 286);
+            this.labelOutputPerm.Name = "labelOutputPerm";
+            this.labelOutputPerm.Size = new System.Drawing.Size(0, 20);
+            this.labelOutputPerm.TabIndex = 9;
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(199, 148);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(214, 20);
+            this.textBoxUserName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(82, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "User Name:";
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonModify.Location = new System.Drawing.Point(245, 191);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(75, 23);
+            this.buttonModify.TabIndex = 12;
+            this.buttonModify.Text = "Modify";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // FSPform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(505, 351);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.labelOutputPerm);
+            this.Controls.Add(this.comboBoxPermission);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxDrive);
+            this.Controls.Add(this.label1);
+            this.Name = "FSPform";
+            this.Text = "Folder Security Permission";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxDrive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxPermission;
+        private System.Windows.Forms.Label labelOutputPerm;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonModify;
+    }
+}
+
